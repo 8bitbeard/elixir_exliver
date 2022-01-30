8083b9ef8219b31a3571d386d92b3ec47bb03d23
Wilton Souza <wilton.souza@petlove.com.br>
2022-01-30 14:03:54 -0300

    Adding Order tests

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -2,7 +2,7 @@ defmodule Exlivery.Factory do
   use ExMachina
 
   alias Exlivery.Users.User
-  alias Exlivery.Orders.Item
+  alias Exlivery.Orders.{Item, Order}
 
   def user_factory do
     %User{
@@ -20,6 +20,23 @@ defmodule Exlivery.Factory do
       category: :pizza,
       quantity: 1,
       unit_price: Decimal.new("35.5")
+    }
+  end
+
+  def order_factory do
+    %Order{
+      delivery_address: "Rua das bananeiras, 35",
+      items: [
+        build(:item),
+        build(:item,
+          category: :japosena,
+          description: "Temaki de atum",
+          quantity: 2,
+          unit_price: Decimal.new("20.50")
+        )
+      ],
+      total_price: Decimal.new("76.50"),
+      user_cpf: "12345678900"
     }
   end
 end
