f23e341891a4626a183eb9474536ab7ba5ee296b
Wilton Souza <wilton.souza@petlove.com.br>
2022-01-30 18:01:26 -0300

    Implementing tests for CreateOrUpdate module from Users

---
+++ test/users/create_or_update_test.exs
@@ -0,0 +1,48 @@
+defmodule Exlivery.Users.CreateOrUpdateTest do
+  use ExUnit.Case
+
+  alias Exlivery.Users.Agent, as: UserAgent
+  alias Exlivery.Users.CreateOrUpdate
+
+  import Exlivery.Factory
+
+  describe "call/1" do
+    setup do
+      UserAgent.start_link(%{})
+
+      :ok
+    end
+
+    test "when all params are valid, saves the user" do
+      params = %{
+        name: "Wilton",
+        email: "exmaple@example.com",
+        address: "Rua exemplo, 15",
+        cpf: "12345678900",
+        age: 30
+      }
+
+      response = CreateOrUpdate.call(params)
+
+      expected_response = {:ok, "User created or updated successfully"}
+
+      assert response == expected_response
+    end
+
+    test "when there are invalid params, returns an error" do
+      params = %{
+        name: "Wilton",
+        email: "exmaple@example.com",
+        address: "Rua exemplo, 15",
+        cpf: "12345678900",
+        age: 10
+      }
+
+      response = CreateOrUpdate.call(params)
+
+      expected_response = {:error, "Invalid parameters"}
+
+      assert response == expected_response
+    end
+  end
+end
